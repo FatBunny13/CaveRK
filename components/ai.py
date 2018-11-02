240d886842fccabe50820b1e17d790cea133041c
FatBunny13 <40076909+FatBunny13@users.noreply.github.com>
2018-11-02 07:45:17 -0400

    Add files via upload

--- components/ai.py
+++ components/ai.py
@@ -5,36 +5,71 @@ from random import randint
 from game_messages import Message
 
 
+class PeacefulMonster:
+    def take_turn(self, target, fov_map, game_map, entities):
+        monster = self.owner
+        results = []
+
+        random_x = self.owner.x + randint(0, 2) - 1
+        random_y = self.owner.y + randint(0, 2) - 1
+
+        if random_x != self.owner.x and random_y != self.owner.y:
+            self.owner.move_towards(random_x, random_y, game_map, entities)
+        return results
+
 class BasicMonster:
     def take_turn(self, target, fov_map, game_map, entities):
         results = []
 
         monster = self.owner
-        if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
+        closest_distance = 10
+        for entity in entities:
+            npc_target = entity
+        if monster.fighter.is_peaceful == True and npc_target.ai:
 
-            if monster.distance_to(target) >= 2:
-                if target.fighter.stealthed == 0:
+            distance = monster.distance_to(npc_target)
+
+            if distance < closest_distance:
+                target = npc_target
+                closest_distance = distance
+                if monster.distance_to(target) >= 2:
                     monster.move_astar(target, entities, game_map)
+                elif target.fighter.hp > 0:
+                    attack_results = monster.fighter.attack(target)
+                    results.extend(attack_results)
+            else:
+                random_x = self.owner.x + randint(0, 2) - 1
+                random_y = self.owner.y + randint(0, 2) - 1
+
+                if random_x != self.owner.x and random_y != self.owner.y:
+                    self.owner.move_towards(random_x, random_y, game_map, entities)
+        else:
+                if monster.fighter.is_peaceful == False and libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
+
+                    if monster.distance_to(target) >= 2:
+                        if target.fighter.stealthed == 0:
+                            monster.move_astar(target, entities, game_map)
+                        else:
+                            random_x = self.owner.x + randint(0, 2) - 1
+                            random_y = self.owner.y + randint(0, 2) - 1
+
+                            if random_x != self.owner.x and random_y != self.owner.y:
+                                self.owner.move_towards(random_x, random_y, game_map, entities)
+                    elif target.fighter.hp > 0:
+                        attack_results = monster.fighter.attack(target)
+                        results.extend(attack_results)
                 else:
                     random_x = self.owner.x + randint(0, 2) - 1
                     random_y = self.owner.y + randint(0, 2) - 1
 
                     if random_x != self.owner.x and random_y != self.owner.y:
                         self.owner.move_towards(random_x, random_y, game_map, entities)
-            elif target.fighter.hp > 0:
-                attack_results = monster.fighter.attack(target)
-                results.extend(attack_results)
-        else:
-            random_x = self.owner.x + randint(0, 2) - 1
-            random_y = self.owner.y + randint(0, 2) - 1
-
-            if random_x != self.owner.x and random_y != self.owner.y:
-                self.owner.move_towards(random_x, random_y, game_map, entities)
 
         return results
 
 class SleepMonster:
     def take_turn(self, target, fov_map, game_map, entities):
+        global attacks
         results = []
 
         monster = self.owner
@@ -50,12 +85,15 @@ class SleepMonster:
 
                     if random_x != self.owner.x and random_y != self.owner.y:
                         self.owner.move_towards(random_x, random_y, game_map, entities)
+                        attacks = randint(1, 10)
             elif target.fighter.hp > 0 and target.fighter.paralysis == 0 and 8 <= attacks >= 9 <= attacks <= 10:
                 attack_results = monster.fighter.paralysis_attack(target)
                 results.extend(attack_results)
+                attacks = randint(1, 10)
             elif target.fighter.hp > 0:
                 attack_results = monster.fighter.attack(target)
                 results.extend(attack_results)
+                attacks = randint(1, 10)
         else:
             random_x = self.owner.x + randint(0, 2) - 1
             random_y = self.owner.y + randint(0, 2) - 1
@@ -64,6 +102,43 @@ class SleepMonster:
                 self.owner.move_towards(random_x, random_y, game_map, entities)
 
         return results
+
+class HasteSelfMonster:
+    def take_turn(self, target, fov_map, game_map, entities):
+        global attacks
+        results = []
+
+        monster = self.owner
+        attacks = randint(1,10)
+        if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
+
+            if monster.distance_to(target) >= 2:
+                if target.fighter.stealthed == 0:
+                    monster.move_astar(target, entities, game_map)
+                else:
+                    random_x = self.owner.x + randint(0, 2) - 1
+                    random_y = self.owner.y + randint(0, 2) - 1
+
+                    if random_x != self.owner.x and random_y != self.owner.y:
+                        self.owner.move_towards(random_x, random_y, game_map, entities)
+                        attacks = randint(1, 10)
+            elif monster.fighter.haste == False and 5 <= attacks:
+                attack_results = monster.fighter.haste_self()
+                results.extend(attack_results)
+                attacks = randint(1, 10)
+            elif target.fighter.hp > 0:
+                attack_results = monster.fighter.attack(target)
+                results.extend(attack_results)
+                attacks = randint(1, 10)
+        else:
+            random_x = self.owner.x + randint(0, 2) - 1
+            random_y = self.owner.y + randint(0, 2) - 1
+
+            if random_x != self.owner.x and random_y != self.owner.y:
+                self.owner.move_towards(random_x, random_y, game_map, entities)
+
+        return results
+
 
 class CharmedMonster:
     def __init__(self, previous_ai, number_of_turns=10):
@@ -77,7 +152,7 @@ class CharmedMonster:
         closest_distance = 10
         if self.number_of_turns > 0:
             for entity in entities:
-                if entity.ai and entity != monster and libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+                if entity.ai and entity.ai != PeacefulMonster and entity != monster and libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
                     distance = monster.distance_to(entity)
 
                     if distance < closest_distance:
